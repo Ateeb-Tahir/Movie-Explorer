8c72137a9aa1d408b4367b13aad4efa5365271a1
Ateeb-Tahir <ateebtahir481@gmail.com>
2025-07-13 03:20:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-    const API_KEY = '2305a6cd45a815f5d91132814694c94d'; // Replace with your actual TMDb API key
+    const API_KEY = 'Api_key'; // Replace with your actual TMDb API key
     const BASE_URL = 'https://api.themoviedb.org/3';
     const IMAGE_BASE = 'https://image.tmdb.org/t/p/w500';
